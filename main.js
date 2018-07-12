9f54dc9adebe089fafe82a648df4c2a628cd3b64
oussamaty <oussamaty62@gmail.com>
2018-07-12 22:35:27 +0100

    A simple website

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log("Hello World!");
 document.getElementById("BUTTON").addEventListener("click",function() {
     var randomNumber = Math.floor(10000*Math.random());
-    var text = "This site generate a random series of 4 numbers <br>" 
+    var text = "This site generate a random sequence of 4 numbers <br>" 
     document.getElementById("TEXT").innerHTML = text + randomNumber;
 });
